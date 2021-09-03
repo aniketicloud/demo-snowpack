4b5c32ff0c730a6463212f4e33740f2a97e14072
Aniket Pandharabale <aniketicloud@gmail.com>
2021-09-03 18:51:29 +0530

    using npm mackages

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import { helloWorld } from './hello-world.js';
+import confetti from 'canvas-confetti';
 
 helloWorld();
+confetti.create(document.getElementById('canvas'), {
+  resize: true,
+  useWorker: true,
+})({ particleCount: 200, spread: 200 });
