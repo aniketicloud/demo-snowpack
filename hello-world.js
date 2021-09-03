0f5267c8cb1bd0d077b7b7dae02993113b8f60e1
Aniket Pandharabale <aniketicloud@gmail.com>
2021-09-03 18:42:14 +0530

    using javascript

---
+++ hello-world.js
@@ -0,0 +1,8 @@
+// my-first-snowpack/hello-world.js
+export function helloWorld() {
+  console.log('Hello World!');
+}
+
+// export const helloWorld = () => {
+//   console.log('Hello World!');
+// };
